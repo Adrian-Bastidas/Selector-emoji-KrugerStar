9f947e5b5e58821bacb6888172286f9427a5e3af
magobasti <abastidas94@gmail.com>
2023-01-07 15:39:15 -0500

    cambio de icono de pestaña

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import EmojiPickerInput from './components/emojiPicker/EmojiPickerInput';
 
 function App() {
   return (
+    <>
     <div className="App">
       <EmojiPickerInput/>
     </div>
+    </>
   );
 }
 
