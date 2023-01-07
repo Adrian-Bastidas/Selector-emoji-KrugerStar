bb93c5882c653c899d485a8864d936e77cd29eec
magobasti <abastidas94@gmail.com>
2023-01-07 12:30:30 -0500

    Creación de componente data
    Componente que contiene los emojis y sus palabras clave para la referencia

---
+++ src/components/emojiPicker/data.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const data = [
+    {
+        symbol: "😊",
+        name:"cara alegre",
+        keywords:"smiling face happy",
+    },
+    {
+        symbol: "❤️",
+        name:"corazon",
+        keywords:"heart love like",
+    },
+    {
+        symbol: "🔥",
+        name:"fuego",
+        keywords:"fire hot burn",
+    },
+    {
+        symbol: "💎",
+        name:"diamante",
+        keywords:"gem stone siamond",
+    },
+    {
+        symbol: "🤯",
+        name:"cabeza explotada",
+        keywords:"head exploding mind blowing",
+    },
+    {
+        symbol: "😍",
+        name:"cara enamorada",
+        keywords:"love head love head",
+    },
+    {
+        symbol: "🎶",
+        name:"musica",
+        keywords:"musical notes music",
+    },
+    {
+        symbol: "😂",
+        name:"cara riendo",
+        keywords:"laughter laughing face",
+    },
+    {
+        symbol: "🙈",
+        name:"mono cubriendose",
+        keywords:"monkey distressed monkey",
+    },
+    {
+        symbol: "😑",
+        name:"cara sospechando",
+        keywords:"suspicious face head",
+    },
+    {
+        symbol: "👽",
+        name:"alien",
+        keywords:"alien head",
+    },
+    {
+        symbol: "🐎",
+        name:"caballo",
+        keywords:"horse",
+    },
+]
+
+export default data
